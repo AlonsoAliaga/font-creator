2f4ced4ea096e63eed52bad07f4d4d3a2d0ea5ba
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-05-30 07:07:23 -0500

    Updated index.html

--- docs/script.js
+++ docs/script.js
@@ -524,7 +524,7 @@ function loadCounter() {
     let link = atob("aHR0cHM6Ly9hbG9uc29hcGkuZGlzY2xvdWQuYXBwL2NvdW50ZXI/c2l0ZT08c2l0ZT4ma2V5PTxrZXk+")
      .replace(/<site>/g,"font-creator").replace(/<key>/g,"KEY-A");
     let counter = document.getElementById("visitor-counter");
-    console.log(link)
+    //console.log(link)
     if(counter) {
       $.ajax({
         url: link,
